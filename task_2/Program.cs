46d96fac43fa98d6c395d7d31845522155621eb4
Maria <m.shklyaeva96@gmail.com>
2022-12-03 02:35:25 +0500

    decision of task_2

---
+++ task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("введите первое число");
+string num_1=Console.ReadLine()!;
+Console.WriteLine("введите второе число");
+string num_2=Console.ReadLine()!;
+int a=int.Parse (num_1);
+int b=int.Parse (num_2);
+
+int max=a;
+
+if (a>max) max=a;
+if (b>max) max=b;
+
+Console.Write ("max=");
+Console.WriteLine (max);

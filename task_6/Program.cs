72da3c3a45ec8dedb5fdae6fa091b579836fe0e4
Maria <m.shklyaeva96@gmail.com>
2022-12-03 03:20:44 +0500

    decision of task_6

---
+++ task_6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите число");
+string input = Console.ReadLine()!;
+int num = int.Parse(input);
+
+if (num%2==0)
+{
+    Console.WriteLine("Yes");
+}
+if (num%2==1)
+{
+    Console.WriteLine("No");
+}
